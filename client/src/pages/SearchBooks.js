86d5a879d8ea715c0a77bbeb25561d21cc40ea8e
Katelyn Bunker <kerbunker@gmail.com>
2022-03-12 22:19:14 -0500

    Worked on search and save books. not working right yet

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
+import { useMutation, useQuery } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -13,6 +16,17 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, {error}] = useMutation(SAVE_BOOK, {
+    update(cache, {data: { bookToSave } }) {
+      const { me } = cache.readQuery({ query: GET_ME });
+
+      cache.writeQuery({
+        query: GET_ME,
+        data: { me: { ...me, saveBooks: [...me.savedBooks, ...bookToSave] } }
+      });
+    }
+  });
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,7 +79,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { input: bookToSave }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
